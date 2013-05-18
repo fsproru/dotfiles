844f5a76156956748735386e4f44f23cb532a345
Alexander Tamoykin <at@zestcash.com>
2013-05-17 19:07:28 -0700

    Added copy/paste and resizing windows key mappings for emacs

--- symlink/emacs.d/init.el
+++ symlink/emacs.d/init.el
@@ -1,6 +1,6 @@
-;;
-;; This is the first thing to get loaded.
-;; 
+;;----------------------------------------;;
+;; This is the first thing to get loaded. ;;
+;; ---------------------------------------;;
 
 ;; elpa package manager
 (setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
@@ -20,12 +20,53 @@
                             (interactive)
                             (scroll-up 1)))
 
+;; color theme
+(load-theme 'solarized-dark t)
+
+;; Copy and Paste
+(defun copy-from-osx ()
+(shell-command-to-string "pbpaste"))
+
+(defun paste-to-osx (text &optional push)
+(let ((process-connection-type nil))
+(let ((proc (start-process "pbcopy" "*Messages*" "pbcopy")))
+(process-send-string proc text)
+(process-send-eof proc))))
+
+;; Override defaults to use the mac copy and paste
+(setq interprogram-cut-function 'paste-to-osx)
+(setq interprogram-paste-function 'copy-from-osx)
+
 ;; start in server mode 
 (load "server")
 (unless (server-running-p) (server-start))
 
-;; color theme
-(load-theme 'solarized-dark t)
+;; multi-term
+(require 'multi-term)
+(setq multi-term-program "/bin/bash")
+(setq multi-term-dedicated-select-after-open-p t)
+(when (require 'term nil t) ; only if term can be loaded..
+  (global-set-key (kbd "<f5>") 'multi-term-dedicated-toggle)
+  (setq term-bind-key-alist
+        (list (cons "C-c C-c" 'term-interrupt-subjob)
+              (cons "C-p" 'previous-line)
+              (cons "C-n" 'next-line)
+              (cons "M-f" 'term-send-forward-word)
+              (cons "M-b" 'term-send-backward-word)
+              (cons "C-c C-j" 'term-line-mode)
+              (cons "C-c C-k" 'term-char-mode)
+              (cons "M-DEL" 'term-send-backward-kill-word)
+              (cons "M-d" 'term-send-forward-kill-word)
+              (cons "C-r" 'term-send-reverse-search-history)
+              (cons "M-p" 'term-send-raw-meta)
+              (cons "M-y" 'term-send-raw-meta)
+              (cons "C-y" 'term-send-raw))))
 
 ;; makes shift-up work in a terminal
 (define-key input-decode-map "\e[1;2A" [S-up])
+
+;; resizing windows
+(global-set-key (kbd "S-C-<left>") 'shrink-window-horizontally)
+(global-set-key (kbd "S-C-<right>") 'enlarge-window-horizontally)
+(global-set-key (kbd "S-C-<down>") 'shrink-window)
+(global-set-key (kbd "S-C-<up>") 'enlarge-window)
