4140909a8b3e4b4cc4647b4f60abbea545b019c2
Alexander Tamoykin <at@zestcash.com>
2013-05-23 23:24:41 -0700

    Multiterm key bindings, jabber, gnus

--- symlink/emacs.d/init.el
+++ symlink/emacs.d/init.el
@@ -46,21 +46,25 @@
 (setq multi-term-program "/bin/bash")
 (setq multi-term-dedicated-select-after-open-p t)
 (when (require 'term nil t) ; only if term can be loaded..
-  (global-set-key (kbd "<f5>") 'multi-term-dedicated-toggle)
+  (global-set-key (kbd "<f8>") 'multi-term-dedicated-toggle)
+  (global-set-key (kbd "<f7>") 'multi-term-next)
+  (global-set-key (kbd "<f9>") 'multi-term-prev)
+  (global-set-key (kbd "<f6>") 'multi-term)  
+
   (setq term-bind-key-alist
-        (list (cons "C-c C-c" 'term-interrupt-subjob)
-              (cons "C-p" 'previous-line)
-              (cons "C-n" 'next-line)
-              (cons "M-f" 'term-send-forward-word)
-              (cons "M-b" 'term-send-backward-word)
-              (cons "C-c C-j" 'term-line-mode)
-              (cons "C-c C-k" 'term-char-mode)
-              (cons "M-DEL" 'term-send-backward-kill-word)
-              (cons "M-d" 'term-send-forward-kill-word)
-              (cons "C-r" 'term-send-reverse-search-history)
-              (cons "M-p" 'term-send-raw-meta)
-              (cons "M-y" 'term-send-raw-meta)
-              (cons "C-y" 'term-send-raw))))
+	(list (cons "C-c C-c" 'term-interrupt-subjob)
+	 (cons "C-p" 'previous-line)
+	 (cons "C-n" 'next-line)
+	 (cons "M-f" 'term-send-forward-word)
+	 (cons "M-b" 'term-send-backward-word)
+	 (cons "C-c C-j" 'term-line-mode)
+	 (cons "C-c C-k" 'term-char-mode)
+	 (cons "M-DEL" 'term-send-backward-kill-word)
+	 (cons "M-d" 'term-send-forward-kill-word)
+	 (cons "C-r" 'term-send-reverse-search-history)
+	 (cons "M-p" 'term-send-raw-meta)
+	 (cons "M-y" 'term-send-raw-meta)
+	 (cons "C-y" 'term-paste))))
 
 ;; makes shift-up work in a terminal
 (define-key input-decode-map "\e[1;2A" [S-up])
@@ -71,12 +75,44 @@
 (global-set-key (kbd "S-C-<down>") 'shrink-window)
 (global-set-key (kbd "S-C-<up>") 'enlarge-window)
 
+;; chat
+(setq jabber-account-list
+     '(("at@zestfinance.com"
+     (:network-server . "talk.google.com")
+     (:connection-type . ssl)
+     (:port . 443))))
+(add-hook 'jabber-chat-mode-hook 'flyspell-mode)
+
+;; email
+(setq gnus-select-method '(nnimap "gmail"
+				  (nnimap-address "imap.gmail.com")
+				  (nnimap-server-port 993)
+				  (nnimap-stream ssl)))
+(setq message-send-mail-function 'smtpmail-send-it
+      user-mail-address "at@zestfinance.com"
+      smtpmail-starttls-credentials '(("smtp.gmail.com" 587 nil nil))
+      smtpmail-auth-credentials '(("smtp.gmail.com" 587 "at@zestfinance.com" nil))
+      smtpmail-default-smtp-server "smtp.gmail.com"
+      smtpmail-smtp-server "smtp.gmail.com"
+      smtpmail-smtp-service 587
+      smtpmail-local-domain "zestfinance.com"
+      smtpmail-debug-info t)
+(setq gnus-posting-styles
+      '((".*"
+	 (name "Alexader Tamoykin")
+	 (address "at@zestfinance.com"))))
+(add-hook 'message-mode-hook 'flyspell-mode)
+
+;; customization
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(grep-find-ignored-directories (quote ("SCCS" "RCS" "CVS" "MCVS" ".svn" ".git" ".hg" ".bzr" "_MTN" "_darcs" "{arch}" "log" "vendor/bundle" ".idea" "coverage" "jmeter"))))
+ '(grep-find-ignored-directories (quote ("SCCS" "RCS" "CVS" "MCVS" ".svn" ".git" ".hg" ".bzr" "_MTN" "_darcs" "{arch}" "log" "vendor/bundle" ".idea" "coverage" "jmeter")))
+ '(jabber-alert-presence-hooks nil)
+ '(jabber-auto-reconnect t)
+ '(password-cache-expiry 604800))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
