bd90e11bf259b61ee032b0bb60f30916d6491766
Alex Tamoykin <alex@rightbalance.io>
2025-02-16 23:11:37 -0800

    Clean up unused packages

--- install.sh
+++ install.sh
@@ -39,7 +39,7 @@ fi
 
 if ! type brew > /dev/null; then
   echo "=== Installing Homebrew"
-  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
 
 echo "=== Installing Homebrew packages"
